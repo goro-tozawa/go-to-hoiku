17ca338ea853bec9a22f8f1238f60fd717b7f894
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-25 15:55:16 +0900

    空のcommit

---
+++ app/models/qualification.rb
@@ -0,0 +1,14 @@
+class Qualification  < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '保育士' },
+    { id: 3, name: '幼稚園教諭' },
+    { id: 4, name: '調理師' },
+    { id: 5, name: '栄養士' },
+    { id: 6, name: '特になし' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+
+  end
