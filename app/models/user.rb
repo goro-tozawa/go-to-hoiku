1791f17f8c70b957552f4727dedb8997e9fdfc1f
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-27 11:22:25 +0900

    空のコミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  # extend ActiveHash::Associations::ActiveRecordExtensions
+ 
   # belongs_to :qualification
   # has_many :works
   belongs_to :childcare_user
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :qualification_id, numericality: { other_than: 1 } 
 end
