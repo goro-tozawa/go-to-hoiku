d1cb1e3c62591af29f9bfe3d4844fbe73e9c074c
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-27 11:36:11 +0900

    空のコミット

---
+++ app/controllers/childcare_users_controller.rb
@@ -0,0 +1,2 @@
+class ChildcareUsersController < ApplicationController
+end
---
+++ test/controllers/childcare_users_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ChildcareUsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
