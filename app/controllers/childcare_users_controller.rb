f2853a8da2167708ba2fc76aaefed1f10d3f01d0
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-28 18:20:58 +0900

    施設側の新規登録

--- app/controllers/childcare_users_controller.rb
+++ app/controllers/childcare_users_controller.rb
@@ -1,5 +1,13 @@
 class ChildcareUsersController < ApplicationController
   def index
-
   end
+ 
+  def new
+    # @childcare_user = Childcare_user.new(childcare_user_params)
+  end
+
+#   private
+#   def childcare_user_params
+#     params.require(:childcare_user).permit(:facility_name, :facility_name_kana, :business_form, :facility_address, :facility_phone_number, :representative :representative_phone_number, :price,:times_id, :encrypted_password, :self_introduction)
+#  end
 end
