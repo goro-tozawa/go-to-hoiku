a9fe056ef6781784dac6c191735c18c39b633c1f
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-29 14:18:10 +0900

    空のコミット

--- app/controllers/childcare_users_controller.rb
+++ app/controllers/childcare_users_controller.rb
@@ -1,7 +1,9 @@
 class ChildcareUsersController < ApplicationController
   def index
+    @childcare_user = ChildcareUser.new
   end
   def new
+    @childcare_user = ChildcareUser.new
   end
   def create
   end
