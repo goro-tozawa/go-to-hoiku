0c5ce1e7854fd3436b2ddd4c3c8ab21f1fbc2134
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-29 14:49:23 +0900

    コミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :childcare_users
-  devise_for :users
+  devise_for :childcare_users, controllers: {
+    sessions:      'childcare_users/sessions',
+    passwords:     'childcare_users/passwords',
+    registrations: 'childcare_users/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
   root to: "tops#index"
   resources :users
   resources :childcare_users
