97ab83c103accb13ab0887f71862334285bc5228
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-29 12:26:02 +0900

    マイグレーションファイル修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :birthday, :nickname, :postal_code, :qualification, :gender, :self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :birthday, :nickname, :postal_code, :qualification, :gender, :self_introduction, :facility_name])
   end
 
   def basic_auth
--- app/controllers/childcare_users_controller.rb
+++ app/controllers/childcare_users_controller.rb
@@ -1,16 +1,8 @@
 class ChildcareUsersController < ApplicationController
   def index
   end
- 
   def new
-    # @childcare_user = Childcare_user.new(childcare_user_params)
   end
-
   def create
   end
-
-#   private
-#   def childcare_user_params
-#     params.require(:childcare_user).permit(:facility_name, :facility_name_kana, :business_form, :facility_address, :facility_phone_number, :representative :representative_phone_number, :price,:times_id, :encrypted_password, :self_introduction)
-#  end
 end
