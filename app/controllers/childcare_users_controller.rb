b1ec830194065773c7415d01bd5d24ce3a20b08e
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-29 16:03:25 +0900

    ルーティングエラーは解消

--- app/controllers/childcare_users_controller.rb
+++ app/controllers/childcare_users_controller.rb
@@ -1,10 +1,6 @@
 class ChildcareUsersController < ApplicationController
   def index
-    @childcare_user = ChildcareUser.new
   end
   def new
-    @childcare_user = ChildcareUser.new
-  end
-  def create
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = User.new
   end
   def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new
   end
 end
