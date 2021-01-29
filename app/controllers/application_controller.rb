94a2fbb7262529472e6656e4f38e7529e5980c2d
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-29 20:04:48 +0900

    空のコミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :birthday, :nickname, :postal_code, :qualification, :gender, :self_introduction, :facility_name])
   end
 
+  def database_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:facility_name, :facility_name_kana, :business_form, :facility_address,:facility_phone_number, :representative,   :representative_phone_number, :times, :self_introduction])
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
--- app/models/childcare_user.rb
+++ app/models/childcare_user.rb
@@ -1,4 +1,7 @@
 class ChildcareUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :facility_name_kana
+  end
 end
