8d633a33bc3fbb2236c26bea5499a3c92f569049
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-30 21:35:04 +0900

    画像投稿機能

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,5 +2,22 @@ class WorksController < ApplicationController
   def index
   end
   def new
+   @work = Work.new
+  end
+
+  def create 
+    @work  = Work.new(work_params)
+
+    if @work.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def work_params
+     params.require(:work).permit(:job_description, :item_name, :details, :category_id, :recruiting_number_id, :recruiting_days, :recruiting_times, :hourly_wage, :image).merge(childcare_user_id: current_user.id)
   end
 end
