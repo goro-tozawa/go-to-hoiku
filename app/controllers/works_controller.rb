874f8611d60f731c28b0fd2c778438941537f0db
goro-tozawa <ralugoro.0905@icloud.com>
2021-02-01 09:18:41 +0900

    仕事依頼　ボタン仮

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -18,6 +18,6 @@ class WorksController < ApplicationController
   private
 
   def work_params
-     params.require(:work).permit(:job_description, :item_name, :details, :category_id, :recruiting_number_id, :recruiting_days, :recruiting_times, :hourly_wage, :image).merge(childcare_user_id: current_user.id)
+     params.require(:work).permit(:job_description, :item_name, :details, :category_id, :person, :recruiting_days, :recruiting_times, :hourly_wage, :image).merge(childcare_user_id: current_user.id)
   end
 end
