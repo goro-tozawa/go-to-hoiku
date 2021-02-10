1cef962c8ccdb3ee500c2752c2172296dd41e8f2
goro-tozawa <ralugoro.0905@icloud.com>
2021-02-10 10:24:05 +0900

    企業バリデーション　完成

--- app/models/childcare_user.rb
+++ app/models/childcare_user.rb
@@ -8,15 +8,13 @@ class ChildcareUser < ApplicationRecord
    validates_format_of :password,with: PASSWORD_REGEX, message: "both letters and numbers"
 
    with_options presence: true do
-      # validates :facility_name,format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "Name is invalid. Input full-width characters."}
-
-      #   validates :facility_name_kana, format:{with: /\A[ァ-ヶー－]+\z/}
-
-      #   validates :business_form
-      #   validates :facility_address
-      #   validates :facility_phone_number, format: { with:/\A\d{11}\z/, message: "Please write within 11 digits with only numbers" }
-      #   validates :representative
-      #   validates :representative_phone_number, format: { with:/\A\d{11}\z/, message: "Please write within 11 digits with only numbers" }
-      #   validates :times
+      validates :facility_name,format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "Name is invalid. Input full-width characters."}
+      validates :facility_name_kana, format:{with: /\A[ァ-ヶー－]+\z/}
+      validates :business_form
+      validates :facility_address,format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "The facility address is invalid. Enter double-byte characters."}
+      validates :facility_phone_number, format: { with:/\A\d{11}\z/, message: "Please write within 11 digits with only numbers" }
+      validates :representative,format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "The representative nameis invalid. Input full-width characters."}
+      validates :representative_phone_number, format: { with:/\A\d{11}\z/, message: "Please write within 11 digits with only numbers" }
+      validates :times
   end
 end
