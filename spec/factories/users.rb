f6e8a9e9cd81856c7fce4ee942cfc24826eada68
goro-tozawa <ralugoro.0905@icloud.com>
2021-03-05 17:09:07 +0900

    テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
      name                                       {"test"}
-     nickname                                       {"test"}
-     birthday                                        {"1111-11-11"}
      email                                       {Faker::Internet.free_email}
      password                                     {"aaaa00"}
-     password_confirmation                    {"aaaa00"}
+     password_confirmation                   {"aaaa00"}
+     nickname                                       {"test"}
+     postal_code                                   {"111-1111"}
      last_name                                     {"テスト"}
      first_name                                    {"テスト"}
      last_kana                                     {"アアア"}
