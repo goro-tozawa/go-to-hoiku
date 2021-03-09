2bce8747a75c734066efeba2767d5ee5f03c7ce2
goro-tozawa <ralugoro.0905@icloud.com>
2021-03-09 16:33:30 +0900

    空のcommit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
      password_confirmation                   {"aaaa00"}
      nickname                                       {"test"}
      postal_code                                   {"111-1111"}
+     qualification                                   {"111-1111"}
      last_name                                     {"テスト"}
      first_name                                    {"テスト"}
      last_kana                                     {"アアア"}
